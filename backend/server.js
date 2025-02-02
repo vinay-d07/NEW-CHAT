5422c70a048d09db0c94ed34f5d0e7c387c9c9a1
vinay-d07 <vinaydhh@gmail.com>
2025-02-02 20:16:32 +0530

    frontend

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,24 @@ const { createServer } = require("http");
 const app = express();
 const server = createServer(app);
 const userModel = require("./models/userModel");
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-
 app.get("/", (req, res) => {
-    console.log(" home ");
-    res.json("home")
+  console.log(" home ");
+  res.json("home");
 });
 
 const ChatRouter = require("./routes/Chat");
+const { Server } = require("socket.io");
 app.use("/chat", ChatRouter);
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  socket.emit(" hello ");
+});
 
 server.listen(3000, () => {
   console.log("everything's fine ");
